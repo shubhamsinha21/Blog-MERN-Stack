3ea7690efb7d9c3b02e36e30fe7b6870bb9087e9
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-11-29 21:45:47 +0530

    Footer + Header + ErrorPage + styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,23 @@ import { AiOutlineClose } from 'react-icons/ai'
 
 function Header() {
     return (
-        <div className='container nav-container'>
-            <Link to='/' className='nav-logo'>
-                <img src={Logo} alt='navbar-logo' />
-            </Link>
-            <ul className="nav-menu">
-                <li><Link to='./profile'>Shubham Sinha</Link></li>
-                <li><Link to='./create'>Create Post</Link></li>
-                <li><Link to='./authors'>Authors</Link></li>
-                <li><Link to='./logout'>Logout</Link></li>
-            </ul>
-            <button className='nav-toggle-btn'>
-                <FaBars />
-                <AiOutlineClose />
-            </button>
-        </div>
+        <nav>
+            <div className='container nav-container'>
+                <Link to='/' className='nav-logo'>
+                    <img src={Logo} alt='navbar-logo' />
+                </Link>
+                <ul className="nav-menu">
+                    <li><Link to='./profile'>Shubham Sinha</Link></li>
+                    <li><Link to='./create'>Create Post</Link></li>
+                    <li><Link to='./authors'>Authors</Link></li>
+                    <li><Link to='./logout'>Logout</Link></li>
+                </ul>
+                <button className='nav-toggle-btn'>
+                    <FaBars />
+                    <AiOutlineClose />
+                </button>
+            </div>
+        </nav>
     )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,6 +7,11 @@ function Layout() {
         <>
             <Header />
             <Outlet />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Possimus vero sequi quasi numquam, ipsum quam labore nulla
+            quae voluptatum harum delectus laudantium illo mollitia nisi n
+            emo quidem temporibus dolor deleniti pariatur veritatis esse! Corporis aut incidunt natus ipsam, nam explicabo nulla quos nemo a asperiores possimus accusamus. Adipisci culpa, eum blanditiis consequuntur vel recusandae dicta dolores error ipsam! Explicabo nihil magnam at, quisquam aut libero placeat quasi suscipit maxime rem cupiditate nemo, alias rerum eaque aliquam repudiandae possimus est ipsam! Odit eaque quia illum, sapiente iure vel fuga at quod culpa deleniti suscipit
+            placeat ab nemo quos itaque neque asperiores?
             <Footer />
         </>
     )
