ea02016f03d003b0b8658390ff7ca0a0ab18a502
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-11-29 15:27:31 +0530

    Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import UserProfile from './pages/UserProfile';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import CategoryPost from './pages/CategoryPost';
+import AuthorPosts from './pages/AuthorPost';
+import Authors from './pages/Authors';
+import Dashboard from './pages/Dashboard';
+import Logout from './pages/Logout';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'posts/:id', element: <PostDetail /> },
+      { path: 'register', element: <Register /> },
+      { path: 'login', element: <Login /> },
+      { path: 'profile/:id', element: <UserProfile /> },
+      { path: 'authors', element: <Authors /> },
+      { path: 'posts/users/:id', element: <AuthorPosts /> },
+      { path: 'create', element: <CreatePost /> },
+      { path: 'posts/categories/:category', element: <CategoryPost /> },
+      { path: 'myposts/:id', element: <Dashboard /> },
+      { path: 'posts/:id/edit', element: <EditPost /> },
+      { path: 'logout', element: <Logout /> },
+
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>hello</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
